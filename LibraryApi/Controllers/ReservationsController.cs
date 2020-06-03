a7a488b84c5d744b5c5a59cf10017f352fff7bcc
Jeffry Gonzalez <jeff@hypertheory.com>
2020-06-03 10:23:04 -0400

    ok

--- LibraryApi/Controllers/ReservationsController.cs
+++ LibraryApi/Controllers/ReservationsController.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryApi.Domain;
+using LibraryApi.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,24 +11,27 @@ namespace LibraryApi.Controllers
     public class ReservationsController : ControllerBase
     {
         LibraryDataContext Context;
+        ISendReservationsToTheQueue Queue;
 
-        public ReservationsController(LibraryDataContext context)
+        public ReservationsController(LibraryDataContext context, ISendReservationsToTheQueue queue)
         {
             Context = context;
+            Queue = queue;
         }
 
         [HttpPost("/reservations")]
         public async Task<ActionResult<Reservation>> AddReservation([FromBody] Reservation reservationToAdd)
         {
             var numberOfBooksInReservation = reservationToAdd.Books.Count(c => c == ',');
-            for(var t =0; t< numberOfBooksInReservation; t++)
-            {
-                await Task.Delay(1000); // take a second to process each book.
-            }
+            //for(var t =0; t< numberOfBooksInReservation; t++)
+            //{
+            //    await Task.Delay(1000); // take a second to process each book.
+            //}
             reservationToAdd.ReservationCreated = DateTime.Now;
             reservationToAdd.Status = ReservationStatus.Processing;
             Context.Reservations.Add(reservationToAdd);
             await Context.SaveChangesAsync();
+            Queue.SendReservationForProcessing(reservationToAdd);
             return Ok(reservationToAdd);
         }
     }
