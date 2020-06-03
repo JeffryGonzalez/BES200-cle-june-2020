22654d73beefcbf8e2ead01e45264c34ddae011a
Jeffry Gonzalez <jeff@hypertheory.com>
2020-06-03 15:56:56 -0400

    added that tye thing

--- LibraryApi/Program.cs
+++ LibraryApi/Program.cs
@@ -14,6 +14,7 @@ namespace LibraryApi
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World");
             CreateHostBuilder(args).Build().MigrateDatabase<LibraryDataContext>().Run();
         }
 
