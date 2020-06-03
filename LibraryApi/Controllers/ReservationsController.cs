c25fbf847ae5c823e997a49eeed972424f5a8326
Jeffry Gonzalez <jeff@hypertheory.com>
2020-06-03 15:40:38 -0400

    done

--- LibraryApi/Controllers/ReservationsController.cs
+++ LibraryApi/Controllers/ReservationsController.cs
@@ -1,6 +1,7 @@
 ﻿using LibraryApi.Domain;
 using LibraryApi.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,7 +33,65 @@ namespace LibraryApi.Controllers
             Context.Reservations.Add(reservationToAdd);
             await Context.SaveChangesAsync();
             Queue.SendReservationForProcessing(reservationToAdd);
-            return Ok(reservationToAdd);
+            return CreatedAtRoute("reservations#get", new { id = reservationToAdd.Id, }, reservationToAdd);
         }
+
+        // After here.
+
+        [HttpGet("/reservations/{id:int}", Name ="reservations#get")]
+        public async Task<ActionResult<Reservation>> GetAReservation(int id)
+        {
+            var reservation = await Context.Reservations.SingleOrDefaultAsync(r => r.Id == id);
+            if(reservation == null)
+            {
+                return NotFound();
+            } else
+            {
+                return Ok(reservation);
+            }
+        }
+
+
+        [HttpGet("/reservations/pending")]
+        public async Task<ActionResult<List<Reservation>>> GetPendingReservations()
+        {
+            var result = await Context.Reservations.Where(r => r.Status == ReservationStatus.Processing).ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("/reservations/approved")]
+        public async Task<ActionResult<List<Reservation>>> GetApprovedReservations()
+        {
+            var result = await Context.Reservations.Where(r => r.Status == ReservationStatus.Approved).ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("/reservations/denied")]
+        public async Task<ActionResult<List<Reservation>>> GetDeniedReservations()
+        {
+            var result = await Context.Reservations.Where(r => r.Status == ReservationStatus.Denied).ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpPost("/reservations/approved")]
+        public async Task<ActionResult> ApproveReservation([FromBody] Reservation reservation)
+        {
+            var storedReservation = await Context.Reservations.Where(r => r.Id == reservation.Id).SingleAsync();
+            storedReservation.Status = ReservationStatus.Approved;
+            await Context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPost("/reservations/denied")]
+        public async Task<ActionResult> DenyReservation([FromBody] Reservation reservation)
+        {
+            var storedReservation = await Context.Reservations.Where(r => r.Id == reservation.Id).SingleAsync();
+            storedReservation.Status = ReservationStatus.Denied;
+            await Context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+
     }
 }
